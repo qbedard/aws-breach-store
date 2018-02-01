e13d72ff6c5c13901d0e956a95cbddc74f1c9f15
Tim Bedard <tpbedard@gmail.com>
2018-02-01 00:10:00 -0600

    lambda_function functions

--- api/email/lambda_function.py
+++ api/email/lambda_function.py
@@ -1,4 +1,5 @@
 import os
+import json
 import boto3
 from aws_requests_auth.aws_auth import AWSRequestsAuth
 from elasticsearch import Elasticsearch, RequestsHttpConnection
@@ -27,6 +28,29 @@ def lambda_handler(event, context):
         connection_class=RequestsHttpConnection
     )
 
-    es_response = es.search(index='identity', body={'query': {'email': email}})
+    es_response = es.search(index='identity', body={
+        'query': {
+            'constant_score': {
+                'filter': {
+                    'term': {
+                        'email': email
+                    }
+                }
+            }
+        }})
 
-    return {'statusCode': '200', 'body': es_response}
+    results = []
+    for hit in es_response['hits']['hits']:
+        results.append(hit['_source'])
+
+    response_body = {'hits': es_response['hits']['total'], 'results': results}
+
+    response = {
+        'isBase64Encoded': False,
+        'statusCode': 200,
+        'headers': {'Access-Control-Allow-Origin': '*',
+                    'Content-Type': 'application/json'},
+        'body': json.dumps(response_body)
+    }
+
+    return response
